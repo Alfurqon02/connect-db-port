09a72aed1dc3f3ac1850e0a8d50ae64b18d939a9
haizk <haizkb@gmail.com>
2024-03-05 18:19:55 +0700

    feat: Add vue page, configure router

---
+++ resources/views/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>MCS</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+    <div id="app">app.blade.php</div>
+    @vite('resources/js/app.js')
+</body>
+
+</html>
