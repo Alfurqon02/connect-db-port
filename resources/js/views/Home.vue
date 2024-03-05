fd82451395316c61f649ed50b144d7adfeb39325
haizk <haizkb@gmail.com>
2024-03-05 22:41:26 +0700

    refactor: Add modal

--- resources/js/views/Home.vue
+++ resources/js/views/Home.vue
@@ -1,25 +1,22 @@
 <script setup>
-import { onMounted, ref } from 'vue'
-import axios from 'axios'
+import { onMounted, ref } from "vue";
+import axios from "axios";
 
-const open = ref(false)
-let items = ref([])
+const open = ref(false);
+let items = ref([]);
 // let props = defineProps(['id'])
 // let name = ref([])
 // let stock = ref([])
 // let category = ref([])
 // const router = useRouter()
 
-
 onMounted(async () => {
-    const response = await axios.get('/api/getData')
-    items.value = response.data.items
-    console.log(items.value)
-})
+    const response = await axios.get("/api/getData");
+    items.value = response.data.items;
+    console.log(items.value);
+});
 
-async function editItem() {
-
-}
+async function editItem() {}
 
 // const editNews = async () => {
 //     // Add validation / CSRF token
@@ -52,122 +49,93 @@ async function editItem() {
 
 <template>
     <main>
-        <!DOCTYPE html>
+        <!doctype html>
         <html lang="en">
+            <head>
+                <meta charset="utf-8" />
+                <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1"
+                />
+                <title>
+                    Bootstrap CRUD Data Table for Database with Modal Form
+                </title>
+            </head>
 
-        <head>
-            <meta charset="utf-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1">
-            <title>Bootstrap CRUD Data Table for Database with Modal Form</title>
-        </head>
-
-        <body>
-            <div class="container">
-                <div class="table-responsive">
-                    <div class="table-wrapper">
-                        <div class="table-title">
-                            <div class="row">
-                                <div class="col-xs-6">
-                                    <h2>Manage <b>Employees</b></h2>
-                                </div>
-                                <div class="col-xs-6">
-                                    <a href="#addEmployeeModal" class="btn btn-success" data-toggle="modal"><i
-                                            class="material-icons">&#xE147;</i> <span>Add New Item</span></a>
+            <body>
+                <div class="container">
+                    <div class="table-responsive">
+                        <div class="table-wrapper">
+                            <div class="table-title">
+                                <div class="row">
+                                    <div class="col-xs-6">
+                                        <h2>Manage <b>Employees</b></h2>
+                                    </div>
+                                    <div class="col-xs-6">
+                                        <a
+                                            href="#addEmployeeModal"
+                                            class="btn btn-success"
+                                            data-toggle="modal"
+                                            ><i class="material-icons"
+                                                >&#xE147;</i
+                                            >
+                                            <span>Add New Item</span></a
+                                        >
+                                    </div>
                                 </div>
                             </div>
+                            <table class="table table-striped">
+                                <thead>
+                                    <tr>
+                                        <!-- <th>#</th> -->
+                                        <th>Name</th>
+                                        <th>Stock</th>
+                                        <th>Category</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr v-for="item in items" :key="item.id">
+                                        <td>{{ item.name }}</td>
+                                        <td>{{ item.stock }}</td>
+                                        <td>{{ item.categories.name }}</td>
+                                        <td>
+                                            <button @click="showModal">
+                                                Edit
+                                            </button>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
                         </div>
-                        <table class="table table-striped">
-                            <thead>
-                                <tr>
-                                    <!-- <th>#</th> -->
-                                    <th>Name</th>
-                                    <th>Stock</th>
-                                    <th>Category</th>
-                                    <th>Action</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr v-for="item in items" :key="item.id">
-                                    <td>{{ item.name }}</td>
-                                    <td>{{ item.stock }}</td>
-                                    <td>{{ item.categories.name }}</td>
-                                    <td>
-                                        <button @click="open = true">Edit</button>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
                     </div>
                 </div>
-            </div>
-            <!-- Edit Modal HTML -->
-            <!-- <div id="addEmployeeModal" class="modal fade">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <form>
-                            <div class="modal-header">
-                                <h4 class="modal-title">Add Employee</h4>
-                                <button type="button" class="close" data-dismiss="modal"
-                                    aria-hidden="true">&times;</button>
-                            </div>
-                            <div class="modal-body">
-                                <div class="form-group">
-                                    <label>Name</label>
-                                    <input type="text" class="form-control" required>
-                                </div>
-                                <div class="form-group">
-                                    <label>Email</label>
-                                    <input type="email" class="form-control" required>
-                                </div>
-                                <div class="form-group">
-                                    <label>Address</label>
-                                    <textarea class="form-control" required></textarea>
-                                </div>
-                                <div class="form-group">
-                                    <label>Phone</label>
-                                    <input type="text" class="form-control" required>
-                                </div>
-                            </div>
-                            <div class="modal-footer">
-                                <input type="button" class="btn btn-default" data-dismiss="modal" value="Cancel">
-                                <input type="submit" class="btn btn-success" value="Add">
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div> -->
-            <!-- Delete Modal HTML -->
-            <!-- <div id="deleteEmployeeModal" class="modal fade">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <form>
-                            <div class="modal-header">
-                                <h4 class="modal-title">Delete Employee</h4>
-                                <button type="button" class="close" data-dismiss="modal"
-                                    aria-hidden="true">&times;</button>
-                            </div>
-                            <div class="modal-body">
-                                <p>Are you sure you want to delete these Records?</p>
-                                <p class="text-warning"><small>This action cannot be undone.</small></p>
-                            </div>
-                            <div class="modal-footer">
-                                <input type="button" class="btn btn-default" data-dismiss="modal" value="Cancel">
-                                <input type="submit" class="btn btn-danger" value="Delete">
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div> -->
-            <!-- Modal Test -->
-            <!-- <Teleport to="body">
-                <div v-if="open" class="modal">
-                    <p>Hello from the modal!</p>
-                    <button @click="open = false">Close</button>
-                </div>
-            </Teleport> -->
-        </body>
-
+                <Modal v-show="isModalVisible" @close="closeModal" />
+            </body>
         </html>
     </main>
 </template>
+
+<script>
+import Modal from "./components/Modal.vue";
+
+export default {
+    components: {
+        Modal,
+    },
+    data() {
+        return {
+            isModalVisible: false,
+        };
+    },
+    methods: {
+        showModal() {
+            this.isModalVisible = true;
+        },
+        closeModal() {
+            this.isModalVisible = false;
+        },
+    },
+};
+</script>
